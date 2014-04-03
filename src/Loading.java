3ec123ae74d816e13a12bbc4397fe8d93c0d059e
takylofr <sebthuret@yahoo.fr>
2014-04-03 17:49:29 +0200

    TakTool 0.5
    -Suppresion des choses inutile

---
+++ src/Loading.java
@@ -0,0 +1,43 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/* ON LOAD TOUTES LES IMAGES */
+
+public class Loading {
+
+	// les icones des stats
+	static ImageIcon cc = new ImageIcon("data/Elements/cc.png");
+	static ImageIcon dTerre = new ImageIcon("data/item/Elements/dTerre.png");
+	static ImageIcon dEau = new ImageIcon("data/item/Elements/dEau.png");
+	static ImageIcon dFeu = new ImageIcon("data/item/Elements/dFeu.png");
+	static ImageIcon dNeutre = new ImageIcon("data/item/Elements/dNeutre.png");
+	static ImageIcon dAir = new ImageIcon("data/item/Elements/dAir.png");
+	static ImageIcon dmg = new ImageIcon("data/item/Elements/dmg.png");
+	static ImageIcon esqPa = new ImageIcon("data/item/Elements/esqPa.png");
+	static ImageIcon esqPm = new ImageIcon("data/item/Elements/esqPm.png");
+	static ImageIcon air = new ImageIcon("data/item/Elements/air.png");
+	static ImageIcon feu = new ImageIcon("data/item/Elements/feu.png");
+	static ImageIcon terre = new ImageIcon("data/item/Elements/terre.png");
+	static ImageIcon neutre = new ImageIcon("data/item/Elements/neutre.png");
+	static ImageIcon vita = new ImageIcon("data/item/Elements/vita.png");
+	static ImageIcon sage = new ImageIcon("data/item/Elements/sage.png");
+	static ImageIcon resAir = new ImageIcon("data/item/Elements/resistance_air.png");
+	static ImageIcon resTerre = new ImageIcon("data/item/Elements/resistance_terre.png");
+	static ImageIcon resEau = new ImageIcon("data/item/Elements/resistance_eau.png");
+	static ImageIcon resFeu = new ImageIcon("data/item/Elements/resistance_feu.png");
+	static ImageIcon resNeutre = new ImageIcon("data/item/Elements/resistance_neutre.png");
+	static ImageIcon invok = new ImageIcon("data/item/Elements/invok.png");
+	static ImageIcon po = new ImageIcon("data/item/Elements/po.png");
+	static ImageIcon pod = new ImageIcon("data/item/Elements/pod.png");
+	static ImageIcon ini = new ImageIcon("data/item/Elements/initiative.png");
+	static ImageIcon pa = new ImageIcon("data/item/Elements/pa.png"); 
+	static ImageIcon pm = new ImageIcon("data/item/Elements/pm.png"); 
+	// les icones du menu
+	static ImageIcon icon_npc = new ImageIcon("data/icone_npc.png");
+	static ImageIcon icon_drop = new ImageIcon("data/icone_ressource.png");
+	static ImageIcon icon_item = new ImageIcon("data/icone_cape.png");
+	// other
+	static JLabel logo_takylo = new JLabel(new ImageIcon("data/tlogo.png"));
+	static JLabel image = new JLabel( new ImageIcon( "data/logo.png"));
+	static ImageIcon img = new ImageIcon("data/icon.png");
+}
